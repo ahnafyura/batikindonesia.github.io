1021517346963b81329e4f673e08df3eb17056d0
ahnafyura <farikatasyafi@gmail.com>
2024-01-19 23:31:50 +0700

    first commit

---
+++ assets/js/api-key.js
@@ -0,0 +1,2 @@
+//Enter your API key here
+let apiKey = "aad2ae4426cf4296b7c68833aef3c65d";
---
+++ assets/js/data.js
@@ -0,0 +1,37 @@
+let shopItemsData = [
+    {
+        id: "jfhgbvnscs",
+        name: "Naga as NFT",
+        price: 2,
+        desc: "Design an alluring neo traditional tattoo artwork featuring a captivating draco's naga as the central character.",
+        img: "images/batikcap.jpg",
+    },
+    {
+        id: "ioytrhndcv",
+        name: "Fox Myth as NFT",
+        price: 3,
+        desc: "Design an alluring neo traditional tattoo artwork featuring a captivating nine tail fox as the central character.",
+        img: "images/img-2.jpg",
+    },
+    {
+        id: "wuefbncxbsn",
+        name: "Midgard as NFT",
+        price: 3,
+        desc: "Design an alluring neo traditional tattoo artwork featuring a captivating demon as the central character.",
+        img: "images/img-3.jpg",
+    },
+    {
+        id: "thyfhcbcv",
+        name: "Dragon as NFT",
+        price: 4,
+        desc: "Design an alluring neo traditional tattoo artwork featuring a captivating dragon as the central character.",
+        img: "images/img-4.jpg",
+    },
+    // {
+    //     id: "thyfhcbca",
+    //     name: "Nightmare Fox as NFT",
+    //     price: 3,
+    //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    //     img: "images/img-5.jpg",
+    // },
+];
